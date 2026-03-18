b02aa899a9d3cde41b436194e164903f2012a6b1
Anantsingh005 <anantsingha411@gmail.com>
2026-03-17 22:45:41 -0700

    Employee Management

---
+++ src/employee/management/system/Splash.java
@@ -0,0 +1,20 @@
+package employee.management.system;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+
+    Splash(){
+
+
+
+        setSize(1170,650);
+        setLayout(null);
+        setLocation(200,100);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
